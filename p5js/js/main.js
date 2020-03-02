3994cac6ed8acc6bb8ad3c3166144f0eabce564f
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-03-02 23:04:15 +0900

    壁紙作成

--- p5js/js/main.js
+++ p5js/js/main.js
@@ -1,37 +1,36 @@
 t=0
-draw = _ => {
+b=2600
+draw=f=>{
   if(!t){
-    createCanvas(w=900,w)
+    createCanvas(w=5120,h=1440)
   }
-  t+=.01
   blendMode(BLEND)
-  background(0,9)
-  // clear()
-
-  // blendMode(DIFFERENCE)
+  background(0,7)
   blendMode(ADD)
+  translate(w/2,h/2)
+  // scale(5,5)
+  noFill()
 
-  translate(w/2,w/2)
-  // scale(.5,.5)
-  // fill(255)
-
+  t+=.003
   i=0
-  while(i++<70){
-  // for (let i = 0; i < 70; ++i) {
-    rotate(.005)
-    j=-7
-    while(j++<9){
-    // for (let j = 0; j < 8; ++j) {
-      b=PI/4*j
-      fill(8,10,20+15*sin(b))
-      s=t-j*.1
-      a=b+1*sin(s)
-      ellipse(w*cos(a)*sin(s/4),w*sin(a)*sin(s/3),15)
-    }
+  while(i++<b){
+    s=sin(t*.27+i)
+    c=cos(t+i*2)
+    // stroke(b/7,b*(c+.8),b*(s+.8),5)
+    // stroke(b*(s+.8),b/7,b*(c+.8),5)
+    stroke(100*(c+1.5),100/7*9,100*(s+1.8),3)
+    a=b*c
+    bezier(
+      b*c,b*s,
+      a*c,a*c,a*s,a*s*s*c,
+      b*s,b*sin(t/3)*c)
   }
-
 }
 
-t=0,draw=_=>{for(t||createCanvas(w=900,w),t+=.01,blendMode(BLEND),background(0,3),blendMode(ADD),translate(w/2,w/2),i=0;i++<70;)for(rotate(.005),j=-7;j++<9;)b=PI/4*j,fill(8,10,20+15*sin(b)),s=t-.1*j,a=b+2*sin(s),ellipse(w*cos(a)*sin(s/4),w*sin(a)*sin(s/3),15)}
+function keyPressed() {
+  if (keyCode === ENTER) {
+    save('image.png')
+  }
+}
 
-// t=0,draw=_=>{for(t||createCanvas(w=900,w),t+=.01,blendMode(BLEND),background(0,3),blendMode(ADD),translate(w/2,w/2),i=0;i++<70;)for(rotate(.005),j=-7;j++<9;)b=PI/4*j,fill(8,10,20+15*sin(b)),s=t-.1*j,a=b+2*sin(s),ellipse(w*cos(a)*sin(s/4),w*sin(a)*sin(s/3),15)}
+// t=0,b=900,draw=(e=>{for(t||createCanvas(b,b),blendMode(BLEND),background(0,9),blendMode(ADD),translate(b/2,b/2),noFill(),t+=.003,i=0;i++<b;)s=sin(.27*t+i),c=cos(t+i),stroke(b*(c+.8),b/7,b*(s+.8),7),a=b*c,bezier(b*c,b*s,a*c,a*c,a*s,a,b*s,b*sin(t/10)*c)});
