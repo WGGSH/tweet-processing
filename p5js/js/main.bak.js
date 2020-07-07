20fd7cde6d06f9d19f33d71cfa56fd3ced1dcc7d
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-07-07 23:52:07 +0900

    0707

--- p5js/js/main.bak.js
+++ p5js/js/main.bak.js
@@ -1,4 +1,3 @@
-import p5 from "p5";
 
 t=0
 
--- p5js/js/minify.js
+++ p5js/js/minify.js
@@ -1,6 +1,26 @@
 const minify = require('minify')
+const fs = require('fs')
+const readline = require('readline')
 
-minify('./js/main.js')
+  const rs = fs.createReadStream('./js/main.js')
+  const ws = fs.createWriteStream('./js/main.bak.js')
+
+const rl = readline.createInterface({
+  input: rs,
+  output: ws,
+})
+
+i=0
+rl.on('line', (lineString) => {
+  if(i++!=0){
+    // console.log(lineString)
+    ws.write(lineString + '\n')
+  }
+})
+
+rl.on('close',() => {})
+
+minify('./js/main.bak.js')
   .then((result)=>{
     console.log('----- minified -----\n')
     console.log(result+'\n')
