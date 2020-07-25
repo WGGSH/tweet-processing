857eb338ad9d0afbf3d40bf53e961143c46e9b67
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-07-25 15:56:30 +0900

    0725

--- p5js/js/result.js
+++ p5js/js/result.js
@@ -3,74 +3,112 @@ t = 0
 
 draw = _ => {
   if(!t++){
-    createCanvas(w=windowWidth,h=windowHeight)
-
-    C=cos
-    S=sin
-
-    p=[0,0,0]
-    n=[0,0,0]
-
-    R=100
-    T=10
-    // drawingContext.shadowColor=color(255,0,0)
-    // drawingContext.shadowBlur=100
+    createCanvas(w=900,w)
+    seed=random(2000)
+    STEP=40
+    BP = [
+      [w/8*4, w/8*1],
+      [w/8*1, w/8*3],
+      [w/8*7, w/8*5],
+      [w/8*4, w/8*7],
+  ]
+    // BP = [
+    //   [w/8*1,w/8*4],
+    //   [w/8*3,w/8*1],
+    //   [w/8*5,w/8*7],
+    //   [w/8*7,w/8*4],
+    // ]
+    R = 3
   }
-  drawingContext.shadowBlur = 0;
-  drawingContext.shadowColor = 'black';
-  translate(w/2,h/2)
-  // clear()
+  randomSeed(seed)
+
+  drawingContext.shadowBlur=0
+  drawingContext.shadowColor='black'
   blendMode(BLEND)
-  background(0,3)
-
+  // clear()
+  background(0,10)
   blendMode(ADD)
-  // fill(255)
 
-  M=P=>{
-    a=PI/3
-    return [
-      P[0]*C(a)+P[1]*C(a*3)+P[2]*C(a*5),
-      P[0]*S(a)+P[1]*S(a*3)+P[2]*S(a*5),
-    ]
+  noFill()
+  fill(255)
+  // stroke(255)
+  noStroke()
+
+  // bezier(w/8,w/2,w/8*3,w/8*3,w/8*5,w/8*5,w/8*7,w/2)
+
+  let bpos =[]
+  for(i=0;i<STEP;i++){
+    step = i/STEP
+    bpos[i] = createVector(
+      bezierPoint(BP[0][0],BP[1][0],BP[2][0],BP[3][0],step),
+      bezierPoint(BP[0][1],BP[1][1],BP[2][1],BP[3][1],step)
+    )
+    // x[i] = bezierPoint(BP[0][0],BP[1][0],BP[2][0],BP[3][0],step)
+    // y[i] = bezierPoint(BP[0][1],BP[1][1],BP[2][1],BP[3][1],step)
+
+    // circle(x,y,10)
+
   }
 
-  N=(p,n)=>{
-    return [
-      p[0]+n[0],
-      p[1]+n[1],
-      p[2]+n[2],
-    ]
+  bvvec = []
+  bvvec[0] = createVector()
+  bvvec[STEP-1] = createVector()
+  for(i=1;i<STEP-1;i++){
+    bvvec[i] = (bpos[i+1].copy().sub(bpos[i-1])).normalize().rotate(-PI/2)
   }
-  stroke(50,50,255)
-  strokeWeight(10)
-  drawingContext.shadowBlur = 10;
-  drawingContext.shadowColor = color(50,50,255);
 
-  l=(t%T)*R/(T-1)
-  pp=[R*M(p)[0],R*M(p)[1]]
-  nv=[M(n)[0]*l,M(n)[1]*l]
-  // np=[pp[0]+M(N(p,n))[0]*l,pp[1]+M(N(p,n))[1]*l]
-  // circle(pp[0],pp[1],20)
+  for(i=1;i<STEP;i++){
+    // strokeWeight((sin(TAU*2/STEP*(i-t/3))+1)*5)
+    // line(bpos[i-1].x,bpos[i-1].y,bpos[i].x,bpos[i].y)
+    npos = bpos[i].copy().add(bvvec[i])
+    // line(bpos[i].x,bpos[i].y,npos.x,npos.y)
+  }
 
-  line(pp[0],pp[1],pp[0]+nv[0],pp[1]+nv[1])
+  beginShape()
 
-  if(t%T==1){
-    let flag=true
-    p=N(p,n)
-    while(flag){
-      flag=true
-      r=Math.floor(random(6))
-      n=[
-        r==0 ? 1 : r==1 ? -1 : 0,
-        r==2 ? 1 : r==3 ? -1 : 0,
-        r==4 ? 1 : r==5 ? -1 : 0,
-      ]
-      if(
-        abs(p[0]+n[0])<4 &&
-        abs(p[1]+n[1])<4 &&
-        abs(p[2]+n[2])<4
-      )flag=false
-    }
+  wf=i=>{
+    let m = t%STEP
+    let b = bezierPoint(
+      1,
+      1/24*23,
+      0,
+      0,
+      (abs(t-i/2)/STEP)%1
+    )
+    // let val = lerp(0,R,Math.min(3/abs(i-t),1))
+    let val = lerp(0,R,b)
+    if(val<R/4*3)val=0
+    return val
+    // return lerp(0,20,Math.min(3/abs(i*2-t/3),1))
+    // return sin(TAU/STEP*Math.min(i+t/30,STEP/2))*10
+  }
+
+  nv=[]
+
+  drawingContext.shadowBlur=10
+  drawingContext.shadowColor=color(58,20,250)
+  for(i=0;i<STEP;i++){
+    W=wf(i)
+    nv[i]=random(40)-20
+    pos=bvvec[i].copy().mult(W+nv[i]).add(bpos[i])
+    vertex(pos.x,pos.y)
+  }
+  for(i=STEP-1;i>=0;--i){
+    W=wf(i)
+    pos=bvvec[i].copy().mult(-W+nv[i]).add(bpos[i])
+    vertex(pos.x,pos.y)
+  }
+  endShape()
+
+
+  if(t%STEP==0){
+    randomSeed(new Date().getTime())
+    BP = [
+      [w/8*(random(4)+2), w/8*1],
+      [w/8*random(8), w/8*3],
+      [w/8*random(8), w/8*5],
+      [w/8*(random(4)+2), w/8*7],
+    ]
   }
 
 }
