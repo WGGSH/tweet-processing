47453a87fae2608614803b709331142d7fc53283
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-08-18 20:48:25 +0900

    0817

--- p5js/js/result.js
+++ p5js/js/result.js
@@ -1,38 +1,105 @@
 
-t = 0
 
+glenable=false
+dc=false
+t = 0
 draw = _ => {
   if(!t++){
-    createCanvas(w=900,w)
-    colorMode(HSB)
-    m=v=>translate(v,v)
-    n=noise
+    createCanvas(w=900,h=900,P2D)
+    // gl = document.getElementById('defaultCanvas0').getContext('webgl');
+    g = createGraphics(w,h,WEBGL)
+
+    // gl = g.getContext('webgl');
+
+
+    img = createImage(iw=128,iw)
+    // img.loadPixels()
+    // colorMode(HSB)
+    // for(y=0;y<iw;y++){
+    //   ay = y/iw * 2 -1
+    //   for(x=0;x<iw;x++){
+    //     ax = x/iw * 2 -1
+    //     img.set(x,y,color(120,10,100-5/(abs(ax)+abs(ay))*10))
+    //   }
+    // }
+    // img.updatePixels()
   }
-  fill(0,0.0)
-  rect(0,0,w,w)
+  img.loadPixels()
+  colorMode(HSB,360,100,100)
+  s=0
+  for(y=0;y<iw;y++){
+    ay = y/iw * 2 -1
+    for(x=0;x<iw;x++){
+      ax = x/iw * 2 -1
+      h = sin(t/80)*50+180
+      s = 100*(sin(s/150)/2+.5)
+      b = 100-4/(abs(pow(ax,4))+abs(pow(ay,4)))*20*(sin(s/150)/2+.5)
+      // b = 100
+      img.set(x,y,color(h,s,b))
+    }
+  }
+  img.updatePixels()
 
-  // m=v=>{
-  //   translate(v,v)
-  // }
-  // m(w/2)
-  m(w/2)
-  rotate(PI/8+sin(n(t/8000)*8))
-  m(-w/2)
+  clear()
 
-  strokeWeight(9)
-  i=w
-  while(i--){
-  // for(i=0;i<w;i++){
-    stroke(99+i%(sin(t/80)*180),70,99,.08)
-    // stroke(120+(t+i)%(120),70,100,.08)
-    // stroke(360/w/4*i+200,50,100)
-    // stroke(sin(i/20)*128,sin((i+20)/20)*128,sin((i+40)/20)*128)
-    point(i,0)
-    // point(i,w-1)
+  g.blendMode(SCREEN)
+
+  g.clear()
+
+  // orbitControl()
+  g.push()
+  g.rotateX(t/200)
+  g.rotateY(t/600)
+  // translate(0,0,t*20)
+  //
+  // translate(0,0,0)
+  if(glenable){
+    gl.disable(gl.DEPTH_TEST);
   }
 
-  copy(0,0,w,w,7,4,w-14,w-8)
+  // g.noFill()
+  // scale(1,1,4/300)
 
+  g.texture(img)
+  g.box(300)
+  g.rotateX(t/500)
+  g.rotateY(t/200)
+  g.box(200)
+  g.pop()
+
+  g.blendMode(BLEND)
+
+  if(glenable){
+    gl.enable(gl.DEPTH_TEST);
+  }
+  g.noStroke()
+  g.fill(0,.05)
+  g.rect(-w/2,-w/2,w,w)
+
+  if(dc){
+    drawingContext.shadowBlur=100
+    drawingContext.shadowColor=color(h,s,b)
+  }
+
+  image(g,0,0)
 }
 
-// t=0,draw=e=>{for(t++||(createCanvas(w=900,w),colorMode(HSB),m=t=>translate(t,t),n=noise),fill(0,0),rect(0,0,w,w),m(w/2),rotate(PI/8+sin(8*n(t/8e3))),m(-w/2),strokeWeight(12),i=0;i++<w;)stroke(120+i%(150*sin(t/80)),70,100,.08),point(i,0);copy(0,0,w,w,7,4,w-14,w-8)}
+mouseClicked=_=>{
+  if(mouseButton==LEFT){
+    glenable=!glenable
+
+    if(glenable){
+      gl = document.getElementById('glgraphics').getContext('webgl');
+    }
+  }
+}
+
+keyPressed=_=>{
+  if(key=='z'){
+    dc=!dc
+    if(!dc){
+      drawingContext.shadowBlur=0
+      drawingContext.shadowColor='black'
+    }
+  }
+}
