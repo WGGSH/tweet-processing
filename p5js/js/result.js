265e168ee2e2f19466a6faf8484bfab99f1ba828
wgg <is0332ks@ed.ritsumei.ac.jp>
2020-08-29 23:50:40 +0900

    0827

--- p5js/js/result.js
+++ p5js/js/result.js
@@ -5,45 +5,41 @@
 t = 0
 draw = _ => {
   if(!t++){
-    createCanvas(w=900,w,WEBGL)
-    R=29
-    N=9
-    // p=pow
-    // colorMode(HSB)
+    createCanvas(w=900,w)
+    // w/=2
+
+    B=blendMode
+    S=sin
+    C=cos
+    N=99
+  }
+  B(BLEND)
+  background(0,8)
+  B(ADD)
+
+  translate(w/2,w/2)
+
+
+  noFill()
+  i=N
+  while(i--){
+  // for(i=0;i<N;i++){
+    // stroke(10,N-i*i,i)
+    stroke(10,N*2-i*4,i)
+    a=t/N*7+i/N
+    b=t/N*8+i/N
+    x=w*C(a)
+    y=w*S(a)
+    // bezier(-w*C(a),-w*S(a),mouseX,mouseY,w/2*S(a),w/2*C(a),w*C(b),w*S(b))
+    bezier(-x,-y,mouseX,mouseY,y/2,x/2,x,y)
+    // a=t/6*3+i/99
+    // b=t/6*2+i/99
+    // bezier(w*cos(a+PI),w*sin(a+PI),100,100,100,100,w*cos(b+PI),w*sin(b+PI))
   }
 
-  // noFill()
-  // stroke(255)
-  normalMaterial()
 
-  // fill(0)
-  // stroke(255)
-
-  clear()
-  orbitControl()
-
-
-  for(x=-N;x<=N;x++){
-    for(y=-N;y<=N;y++){
-      for(z=-N;z<=N;z++){
-        push()
-        translate(R*x,R*y,R*z)
-        // if(0.01/x+y*y+z*z<t%(8*8*8))
-
-        // fill(x*x*y*y*z*z/pow(N,3)*360/8,100,100)
-
-        // if(x*x+y*y+z*z<t)
-        // if((x*x)*(y*y^t-t)/(z*z*z)>(sin(t/20)*50+50)%(p(N,3)))
-        if((x*x)*(y*y^t-t)/(z*z*z)>((sin(t/40)+1)*50)%(pow(N,3)))
-        // if(1*(x*x)*(y*y-t)/(z*z)>(sin(1/t)*100-100)%(pow(N,3)))
-        // if(1*(p(x,2))*(p(y,2))*(p(z,2))>((300*sin(t/30)+150)*1000))
-        // if(1/abs(x)+1/(y)+1/(z)>t/400)
-        // if(tan(x*y*z+t/200) <3)
-        box(R*.97)
-        pop()
-      }
-    }
-  }
 }
 
-t=0,draw=(r=>{for(t++||(createCanvas(w=900,w,WEBGL),R=29,N=9),normalMaterial(),clear(),orbitControl(),x=-N;x<=N;x++)for(y=-N;y<=N;y++)for(z=-N;z<=N;z++)push(),translate(R*x,R*y,R*z),x*x*(y*y^t-t)/(z*z*z)>50*(sin(t/40)+1)%pow(N,3)&&box(.97*R),pop()})
+// t=0,draw=(e=>{for(t++||(createCanvas(w=900,w),p=[],B=blendMode,S=sin,C=cos,N=99),B(BLEND),background(0,8),B(ADD),translate(w/2,w/2),noFill(),i=0;i<N;i++)stroke(N-i,20,i),a=t/N*7+i/N,b=t/N*8+i/N,bezier(-w*C(a),-w*S(a),mouseX,mouseY,w/2*S(a),w/2*C(a),w*C(b),w*S(b))})
+
+// t=0,draw=(e=>{for(t++||(createCanvas(w=900,w),B=blendMode,S=sin,C=cos,N=99),B(BLEND),background(0,8),B(ADD),translate(w/2,w/2),noFill(),i=N;i--;)stroke(10,2*N-4*i,i),a=t/N*7+i/N,b=t/N*8+i/N,x=w*C(a),y=w*S(a),bezier(-x,-y,mouseX,mouseY,y/2,x/2,x,y)});
